7710ffa9bae2569b8a3b4b9514b05126f61117bd
chanchandrue90 <candradwiwaskito90@gmail.com>
2014-03-31 19:24:17 +0700

    add footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+	<footer id="colophon" class="site-footer container" role="contentinfo">
+		<div class="site-info">
+			Proudly powered by <a href="http://ksweb.tif.uad.ac.id/">KSWEB-UAD</a>
+		</div><!-- .site-info -->
+	</footer>
+
+</body>
+</html>
