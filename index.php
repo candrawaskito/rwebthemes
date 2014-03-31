ed644e279f13289f177b467eeb01be313d63677f
chanchandrue90 <candradwiwaskito90@gmail.com>
2014-03-31 23:09:24 +0700

    add the_loop function

--- index.php
+++ index.php
@@ -6,18 +6,24 @@
 
 		<section class="content col-md-9">	
 			<article class="row post">
-			
-				<header class="entry-header">
-					<h1 class="entry-title"><a href="#">Jawa Ipsum nedhi emah-emah ringgit!</a></h1>
+
+			<?php if (have_posts()) {
+				# code...
+				while (have_posts()) {
+					# code...
+					the_post(); ?>
+
+					<header class="entry-header">
+					<h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 					<div class="entry-meta">
 						<span class="entry-date">
-							<a href="" rel="bookmark"><span class="glyphicon glyphicon-calendar"></span> <time class="entry-date" datetime="2014-03-20T14:53:43+00:00">March 20, 2014</time></a>&nbsp;
+							<a href="" rel="bookmark"><span class="glyphicon glyphicon-calendar"></span> <time class="entry-date" datetime="2014-03-20T14:53:43+00:00"><?php the_date(); ?></time></a>&nbsp;
 						</span>
 						<span class="byline">
-							<span class="author vcard"><a class="url fn n" href="" rel="author"><span class="glyphicon glyphicon-user"></span> Admin</a></span>&nbsp;
+							<span class="author vcard"><a class="url fn n" href="<?php get_the_date(); ?>" rel="author"><span class="glyphicon glyphicon-user"></span> <?php the_author(); ?></a></span>&nbsp;
 						</span>
 						<span class="comments-link">
-							<a href="#" title="Comment on oke"><span class="glyphicon glyphicon-comment"></span> Leave a Comment</a>&nbsp;
+							<a href="#" title="Comment on oke"><span class="glyphicon glyphicon-comment"></span>  <?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?></a>&nbsp;
 						</span>
 						<span class="edit-link">
 							<a class="post-edit-link" href="#"><span class="glyphicon glyphicon-edit"></span> Edit Post</a>
@@ -27,9 +33,9 @@
 
 				<div class="entry-content">
 					
-					<p>Jawa Ipsum nedhi emah-emah ringgit jawah sae, leres; kengken kemu; nyukani ajrih ngundang. Manah ijem tapihan putu weteng pamanah tebih nyukani, kajang nanem. Tiapukan, putu rambut pupu; sendhok tanem bojo. Mbekta pados sumerep, nyukani m&eacute;ga awis sekar tuwi katur.</p>
-
-					<p>Dhateng ageng tiyang dhateng, raos mantun kinten. Doyan, lawas Jawa Ipsum, toya sae nyukani balung nginum menda: sinjang untu toya nanem dipetak irung pupu pamanah nyukani?</p>
+					<p>
+						<?php the_content(); ?>
+					</p>
 	
 				</div>
 
@@ -37,76 +43,9 @@
 					<span class="entry-more"><a href="" class="btn btn-primary">READ MORE <span class="glyphicon glyphicon-chevron-right"></span></a></span>
 				</footer>
 
-			</article>
-
-			<article class="row post">
+				<?php }
+			} ?>
 			
-				<header class="entry-header">
-					<h1 class="entry-title"><a href="#">Jawa Ipsum nedhi emah-emah ringgit!</a></h1>
-					<div class="entry-meta">
-						<span class="entry-date">
-							<a href="" rel="bookmark"><span class="glyphicon glyphicon-calendar"></span> <time class="entry-date" datetime="2014-03-20T14:53:43+00:00">March 20, 2014</time></a>&nbsp;
-						</span>
-						<span class="byline">
-							<span class="author vcard"><a class="url fn n" href="" rel="author"><span class="glyphicon glyphicon-user"></span> Admin</a></span>&nbsp;
-						</span>
-						<span class="comments-link">
-							<a href="#respond" title="Comment on oke"><span class="glyphicon glyphicon-comment"></span> Leave a Comment</a>&nbsp;
-						</span>
-						<span class="edit-link">
-							<a class="post-edit-link" href="#"><span class="glyphicon glyphicon-edit"></span> Edit Post</a>
-						</span>
-					</div>
-				</header>
-
-				<div class="entry-content">
-					
-					<p>Jawa Ipsum nedhi emah-emah ringgit jawah sae, leres; kengken kemu; nyukani ajrih ngundang. Manah ijem tapihan putu weteng pamanah tebih nyukani, kajang nanem. Tiapukan, putu rambut pupu; sendhok tanem bojo. Mbekta pados sumerep, nyukani m&eacute;ga awis sekar tuwi katur.</p>
-
-					<p>Dhateng ageng tiyang dhateng, raos mantun kinten. Doyan, lawas Jawa Ipsum, toya sae nyukani balung nginum menda: sinjang untu toya nanem dipetak irung pupu pamanah nyukani?</p>
-	
-				</div>
-
-				<footer class="entry-footer">
-					<span class="entry-more"><a href="" class="btn btn-primary">READ MORE <span class="glyphicon glyphicon-chevron-right"></span></a></span>
-				</footer>
-
-			</article>
-
-			<article class="row post">
-			
-				<header class="entry-header">
-					<h1 class="entry-title"><a href="#">Jawa Ipsum nedhi emah-emah ringgit!</a></h1>
-					<div class="entry-meta">
-						<span class="entry-date">
-							<a href="" rel="bookmark"><span class="glyphicon glyphicon-calendar"></span> <time class="entry-date" datetime="2014-03-20T14:53:43+00:00">March 20, 2014</time></a>&nbsp;
-						</span>
-						<span class="byline">
-							<span class="author vcard"><a class="url fn n" href="" rel="author"><span class="glyphicon glyphicon-user"></span> Admin</a></span>&nbsp;
-						</span>
-						<span class="comments-link">
-							<a href="#respond" title="Comment on oke"><span class="glyphicon glyphicon-comment"></span> Leave a Comment</a>&nbsp;
-						</span>
-						<span class="edit-link">
-							<a class="post-edit-link" href="#"><span class="glyphicon glyphicon-edit"></span> Edit Post</a>
-						</span>
-					</div>
-				</header>
-
-				<div class="entry-content">
-					<p>Jawa Ipsum nedhi emah-emah ringgit jawah sae, leres; kengken kemu; nyukani ajrih ngundang. Manah ijem tapihan putu weteng pamanah tebih nyukani, kajang nanem. Tiapukan, putu rambut pupu; sendhok tanem bojo. Mbekta pados sumerep, nyukani m&eacute;ga awis sekar tuwi katur.</p>
-
-					<p><img src="<?php echo get_template_directory_uri() ?>/img/content-img.jpg" class="alignleft" width="300" />Dhateng ageng tiyang dhateng, raos mantun kinten. Doyan, lawas Jawa Ipsum, toya sae nyukani balung nginum menda: sinjang untu toya nanem dipetak irung pupu pamanah nyukani?</p>
-
-					<p>Jawa Ipsum nedhi emah-emah ringgit jawah sae, leres; kengken kemu; nyukani ajrih ngundang. Manah ijem tapihan putu weteng pamanah tebih nyukani, kajang nanem. Tiapukan, putu rambut pupu; sendhok tanem bojo. Mbekta pados sumerep, nyukani m&eacute;ga awis sekar tuwi katur.</p>
-
-					<p>Dhateng ageng tiyang dhateng, raos mantun kinten. Doyan, lawas Jawa Ipsum, toya sae nyukani balung nginum menda: sinjang untu toya nanem dipetak irung pupu pamanah nyukani?</p>
-
-					<p>Jawa Ipsum nedhi emah-emah ringgit jawah sae, leres; kengken kemu; nyukani ajrih ngundang. Manah ijem tapihan putu weteng pamanah tebih nyukani, kajang nanem. Tiapukan, putu rambut pupu; sendhok tanem bojo. Mbekta pados sumerep, nyukani m&eacute;ga awis sekar tuwi katur.</p>
-
-					<p>Dhateng ageng tiyang dhateng, raos mantun kinten. Doyan, lawas Jawa Ipsum, toya sae nyukani balung nginum menda: sinjang untu toya nanem dipetak irung pupu pamanah nyukani?</p>
-				</div>
-
 			</article>
 
 			<nav class="row navigation nav-post">
