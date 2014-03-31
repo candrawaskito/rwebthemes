c6a7cd8375bc739bded91f117899cf1105e39bdf
chanchandrue90 <candradwiwaskito90@gmail.com>
2014-03-31 19:24:48 +0700

    add sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,23 @@
+<section class="sidebar col-md-3">
+	<aside class="widget">
+		<h3 class="widget-title">Categories</h3>
+
+		<ul>
+			<li><a href="">Technology</a></li>
+			<li><a href="">Life Style</a></li>
+			<li><a href="">Politics</a></li>
+			<li><a href="">Portofolio</a></li>
+		</ul>
+	</aside>
+	
+	<aside class="widget">
+		<h3 class="widget-title">Meta</h3>
+
+		<ul>
+			<li><a href="">Sign In</a></li>
+			<li><a href="">Entries RSS</a></li>
+			<li><a href="">Comments RSS</a></li>
+			<li><a href="">Wordpress.org</a></li>
+		</ul>
+	</aside>	
+</section>
